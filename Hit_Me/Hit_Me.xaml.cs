945c6cf7bd970ef2494648160d985de9d5449508
thtnguyen93 <thtnguyen93@gmail.com>
2017-03-01 22:39:45 -0800

    first commit

---
+++ Hit_Me/Hit_Me.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hit_Me
+{
+    /// <summary>
+    /// Interaction logic for Hit_Me.xaml
+    /// </summary>
+    public partial class Hit_Me : UserControl
+    {
+        public Hit_Me()
+        {
+            InitializeComponent();
+        }
+    }
+}
