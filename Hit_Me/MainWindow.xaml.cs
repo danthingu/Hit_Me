58340ba00caa2b4f27cd2052b311c81b66d489c6
thtnguyen93 <thtnguyen93@gmail.com>
2017-03-02 22:03:55 -0800

    fix some bugs

--- Hit_Me/MainWindow.xaml.cs
+++ Hit_Me/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Hit_Me
         public MainWindow()
         {
             InitializeComponent();
-            bombTimer.Tick += bombTimer_Tick;
+            blockTiming.Tick += blockTimerDrop;
         }
 
         private void canvasBackground_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -35,85 +35,75 @@ namespace Hit_Me
             canvasBackground.Clip = rect;
         }
 
-        // "Adjustments" happen periodically, increasing the speed of bomb
-        // falling and shortening the time between bombs.
-        private DateTime lastAdjustmentTime = DateTime.MinValue;
 
-        // Perform an adjustment every 15 seconds.
+        private DateTime lastAdjustmentTime = DateTime.MinValue;
+        private int droppedCount = 0;
+        private int savedCount = 0;
+
+        private int maxDropped = 5;
         private double secondsBetweenAdjustments = 15;
 
-        // Initially, bombs fall every 1.3 seconds, and hit the ground after 3.5 seconds.
-        private double initialSecondsBetweenBombs = 1.3;
+        private double initialSecondsBetweenBlocks = 1.3;
         private double initialSecondsToFall = 3.5;
-        private double secondsBetweenBombs;
+        private double secondsBetweenBlocks;
         private double secondsToFall;
 
-        // After every adjustment, shave 0.1 seconds off both.
-        private double secondsBetweenBombsReduction = 0.1;
+        private double secondsBetweenBlocksReduction = 0.1;
         private double secondsToFallReduction = 0.1;
 
-        // Make it possible to look up a storyboard based on a bomb.
         private Dictionary<Block, Storyboard> storyboards = new Dictionary<Block, Storyboard>();
 
-        // Fires events on the user interface thread.
-        private DispatcherTimer bombTimer = new DispatcherTimer();
+        private DispatcherTimer blockTiming = new DispatcherTimer();
 
-        // Start the game.
         private void cmdStart_Click(object sender, RoutedEventArgs e)
         {
             cmdStart.IsEnabled = false;
-
-            // Reset the game.
             droppedCount = 0;
             savedCount = 0;
-            secondsBetweenBombs = initialSecondsBetweenBombs;
+            secondsBetweenBlocks = initialSecondsBetweenBlocks;
             secondsToFall = initialSecondsToFall;
-
-            // Start bomb dropping events.            
-            bombTimer.Interval = TimeSpan.FromSeconds(secondsBetweenBombs);
-            bombTimer.Start();
+            blockTiming.Interval = TimeSpan.FromSeconds(secondsBetweenBlocks);
+            blockTiming.Start();
         }
 
-        // Drop a bomb.
-        private void bombTimer_Tick(object sender, EventArgs e)
+        private void blockTimerDrop(object sender, EventArgs e)
         {
-            // Perform an "adjustment" when needed.
             if ((DateTime.Now.Subtract(lastAdjustmentTime).TotalSeconds >
                 secondsBetweenAdjustments))
             {
                 lastAdjustmentTime = DateTime.Now;
 
-                secondsBetweenBombs -= secondsBetweenBombsReduction;
+                secondsBetweenBlocks -= secondsBetweenBlocksReduction;
                 secondsToFall -= secondsToFallReduction;
 
                 // (Technically, you should check for 0 or negative values.
                 // However, in practice these won't occur because the game will
                 // always end first.)
 
-                // Set the timer to drop the next bomb at the appropriate time.
-                bombTimer.Interval = TimeSpan.FromSeconds(secondsBetweenBombs);
+                // Set the timer to drop the next Block at the appropriate time.
+                blockTiming.Interval = TimeSpan.FromSeconds(secondsBetweenBlocks);
 
                 // Update the status message.
                 lblRate.Text = String.Format("A block is released at each {0} seconds.",
-                    secondsBetweenBombs);
+                    secondsBetweenBlocks);
                 lblSpeed.Text = String.Format("Each block takes {0} seconds to fall.",
                     secondsToFall);
             }
 
-            // Create the bomb.
+            // Create the Block.
             Block blocks = new Block();
             blocks.IsFalling = true;
 
-            // Position the bomb.            
+            // Position the Block.            
             Random random = new Random();
             blocks.SetValue(Canvas.LeftProperty,
                 (double)(random.Next(0, (int)(canvasBackground.ActualWidth - 50))));
             blocks.SetValue(Canvas.TopProperty, -100.0);
 
-            // Attach mouse click event (for defusing the bomb).
-            blocks.MouseLeftButtonDown += bomb_MouseLeftButtonDown;
+            // Attach mouse click event (for defusing the Block).
+            blocks.MouseLeftButtonDown += Block_MouseLeftButtonDown;
 
-            // Create the animation for the falling bomb.
+            // Create the animation for the falling Block.
             Storyboard storyboard = new Storyboard();
             DoubleAnimation fallAnimation = new DoubleAnimation();
             fallAnimation.To = canvasBackground.ActualHeight;
@@ -122,19 +112,8 @@ namespace Hit_Me
             Storyboard.SetTarget(fallAnimation, blocks);
             Storyboard.SetTargetProperty(fallAnimation, new PropertyPath("(Canvas.Top)"));
             storyboard.Children.Add(fallAnimation);
-
-            //// Create the animation for the bomb "wiggle."
-            //DoubleAnimation wiggleAnimation = new DoubleAnimation();            
-            //wiggleAnimation.To = 30;
-            //wiggleAnimation.Duration = TimeSpan.FromSeconds(0.2);
-            //wiggleAnimation.RepeatBehavior = RepeatBehavior.Forever;
-            //wiggleAnimation.AutoReverse = true;
-
-            //Storyboard.SetTarget(wiggleAnimation, ((TransformGroup)bomb.RenderTransform).Children[0]);
-            //Storyboard.SetTargetProperty(wiggleAnimation, new PropertyPath("Angle"));
-            //storyboard.Children.Add(wiggleAnimation);
-
-            // Add the bomb to the Canvas.
+   
+            // Add the Block to the Canvas.
             canvasBackground.Children.Add(blocks);
 
             // Add the storyboard to the tracking collection.            
@@ -146,21 +125,21 @@ namespace Hit_Me
             storyboard.Begin();
         }
 
-        private void bomb_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Block_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // Get the bomb.
-            Block bomb = (Block)sender;
-            bomb.IsFalling = false;
+            // Get the Block.
+            Block Block = (Block)sender;
+            Block.IsFalling = false;
 
-            // Get the bomb's current position.
-            Storyboard storyboard = storyboards[bomb];
-            double currentTop = Canvas.GetTop(bomb);
+            // Get the Block's current position.
+            Storyboard storyboard = storyboards[Block];
+            double currentTop = Canvas.GetTop(Block);
 
-            // Stop the bomb from falling.
+            // Stop the Block from falling.
             storyboard.Stop();
 
             // Reuse the existing storyboard, but with new animations.
-            // Send the bomb on a new trajectory by animating Canvas.Top
+            // Send the Block on a new trajectory by animating Canvas.Top
             // and Canvas.Left.
             storyboard.Children.Clear();
 
@@ -169,13 +148,13 @@ namespace Hit_Me
             riseAnimation.To = 0;
             riseAnimation.Duration = TimeSpan.FromSeconds(2);
 
-            Storyboard.SetTarget(riseAnimation, bomb);
+            Storyboard.SetTarget(riseAnimation, Block);
             Storyboard.SetTargetProperty(riseAnimation, new PropertyPath("(Canvas.Top)"));
             storyboard.Children.Add(riseAnimation);
 
             DoubleAnimation slideAnimation = new DoubleAnimation();
-            double currentLeft = Canvas.GetLeft(bomb);
-            // Throw the bomb off the closest side.
+            double currentLeft = Canvas.GetLeft(Block);
+            // Throw the Block off the closest side.
             if (currentLeft < canvasBackground.ActualWidth / 2)
             {
                 slideAnimation.To = -100;
@@ -185,7 +164,7 @@ namespace Hit_Me
                 slideAnimation.To = canvasBackground.ActualWidth + 100;
             }
             slideAnimation.Duration = TimeSpan.FromSeconds(1);
-            Storyboard.SetTarget(slideAnimation, bomb);
+            Storyboard.SetTarget(slideAnimation, Block);
             Storyboard.SetTargetProperty(slideAnimation, new PropertyPath("(Canvas.Left)"));
             storyboard.Children.Add(slideAnimation);
 
@@ -194,24 +173,17 @@ namespace Hit_Me
             storyboard.Begin();
         }
 
-        // Keep track of how many are dropped and stopped.
-        private int droppedCount = 0;
-        private int savedCount = 0;
-
-        // End the game at maxDropped.
-        private int maxDropped = 5;
-
         private void storyboard_Completed(object sender, EventArgs e)
         {
             ClockGroup clockGroup = (ClockGroup)sender;
 
             // Get the first animation in the storyboard, and use it to find the
-            // bomb that's being animated.
+            // Block that's being animated.
             DoubleAnimation completedAnimation = (DoubleAnimation)clockGroup.Children[0].Timeline;
-            Block completedBomb = (Block)Storyboard.GetTarget(completedAnimation);
+            Block completedBlock = (Block)Storyboard.GetTarget(completedAnimation);
 
-            // Determine if a bomb fell or flew off the Canvas after being clicked.
-            if (completedBomb.IsFalling)
+            // Determine if a Block fell or flew off the Canvas after being clicked.
+            if (completedBlock.IsFalling)
             {
                 droppedCount++;
             }
@@ -221,23 +193,23 @@ namespace Hit_Me
             }
 
             // Update the display.
-            lblStatus.Text = String.Format("You have dropped {0} bombs and saved {1}.",
+            lblStatus.Text = String.Format("You have dropped {0} Blocks and saved {1}.",
                 droppedCount, savedCount);
 
             // Check if it's game over.
             if (droppedCount >= maxDropped)
             {
-                bombTimer.Stop();
+                blockTiming.Stop();
                 lblStatus.Text += "\r\n\r\nGame over.";
 
                 // Find all the storyboards that are underway.
                 foreach (KeyValuePair<Block, Storyboard> item in storyboards)
                 {
                     Storyboard storyboard = item.Value;
-                    Block bomb = item.Key;
+                    Block Block = item.Key;
 
                     storyboard.Stop();
-                    canvasBackground.Children.Remove(bomb);
+                    canvasBackground.Children.Remove(Block);
                 }
                 // Empty the tracking collection.
                 storyboards.Clear();
@@ -250,8 +222,8 @@ namespace Hit_Me
                 Storyboard storyboard = (Storyboard)clockGroup.Timeline;
                 storyboard.Stop();
 
-                storyboards.Remove(completedBomb);
-                canvasBackground.Children.Remove(completedBomb);
+                storyboards.Remove(completedBlock);
+                canvasBackground.Children.Remove(completedBlock);
             }
         }
     }
